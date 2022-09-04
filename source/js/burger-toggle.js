cdfaad98cc6e8e12aee932e419bc1d622c1ca1db
Alex Gorodov <a.gorodov.22@gmail.com>
2022-09-04 22:22:31 +0300

    contact section, footer, social and map

--- source/js/burger-toggle.js
+++ source/js/burger-toggle.js
@@ -3,5 +3,10 @@ let navList = document.querySelector('.navigation__list');
 
 burgerButton.onclick = function () {
   burgerButton.classList.toggle('navigation-toggle--opened');
+  if (burgerButton.getAttribute('aria-label') === 'Открыть меню.') {
+    burgerButton.setAttribute('aria-label', 'Закрыть меню.');
+  } else {
+    burgerButton.setAttribute('aria-label', 'Открыть меню.');
+  }
   navList.classList.toggle('navigation__list--opened');
 }
